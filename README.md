f1ae6713698e9c0eccba3d509e13e3ba946920dc
MakotoMiyamoto <makotonomeru@gmail.com>
2023-02-12 02:18:25 -0600

    Initial commit + build script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # college-cpp-stuff
 Repo for stuff I do for college in C++
+
+# How to use
+In order to run examples in the repo, I wrote a build script in Python to
+handle building and running, which you will obviously need Python to run.
+
+This script also requires clang, however in the future I will add support
+for more compilers and probably figure out what compilers work on your machine.
+
+## Usage
+```
+py build.py hello_world.cpp
+```
+Running this (while the current working directory is this repo) will build the
+target file hello_world.cpp and run it, displaying the output wherever it was called.
+
+Output:
+```
+Hello, world!
+```
---
+++ build.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import sys
+
+# (proposal) example invocation:
+# $> py build.py hello_world.cpp -c clang++ -a "-Wall" 
+# $> py build.py stack.cpp -c g++ -a "-O3 -Wall" -std=c++20
+
+if len(sys.argv) < 2 or sys.argv[1] == "--help":
+    print(
+        "This program compiles a file in the examples folder and executes it.",
+        "This program uses clang++ by default, but doesn't yet support specified alternatives nor custom compiler arguments.",
+        "\n"
+        f"{sys.argv[0]} [--help|<src>.cpp]",
+        "",
+        "    --help:\t Show this help screen.",
+        "    <src>.cpp:\t Compile a file in ./examples/<src>.cpp",
+        "",
+        "Example:",
+        f"py {sys.argv[0]} hello_world.cpp",
+        "",
+        sep='\n'
+    )
+    exit(1)
+
+DEBUG_PATH = os.path.join(os.getcwd(), 'debug')
+TARGET_SRC_FILE = os.path.join(os.getcwd(), 'examples', sys.argv[1])
+
+DEFAULT_BIN_PATH_DEBUG = os.path.join(os.getcwd(), 'debug', 'a.exe')
+
+if not os.path.exists(DEBUG_PATH):
+    os.mkdir(DEBUG_PATH)
+
+subprocess.run([
+    "clang++",
+    "-o",
+    DEFAULT_BIN_PATH_DEBUG,
+    TARGET_SRC_FILE,
+    "-Wall"
+])
+
+subprocess.run([DEFAULT_BIN_PATH_DEBUG])
